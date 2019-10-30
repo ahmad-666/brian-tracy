b683541e31f510b094560d93be8cbabb6964384a
ahmad-666 <ahmadrezagghz666@gmail.com>
2019-10-30 16:16:00 -0700

    login but it is both email amd mobile

---
+++ src/signup/signup.js
@@ -0,0 +1,9 @@
+//load styles
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css' ;
+import '../framework.scss' ;
+import '../init.scss' ;
+import '../signup/styles/signup.scss' ;
+//load scripts
+import '../signup/scripts/signup.js' ;
+//load videos 
+
